c2a1d4fead61c67d9a340575015dfab83bca21f8
SportyScripter <adrian.kacorzyk@edu.uekat.pl>
2024-03-16 22:30:33 +0100

    finish task 4 and modify task 3

--- zadania3/zadanie1.c
+++ zadania3/zadanie1.c
@@ -1,12 +1,13 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 
-int factorial(int n) 
-{ 
-    if (n == 0) 
-        return 1; 
-    return n * factorial(n - 1); 
+int factorial(int a){
+    if (a == 0){
+        return 1;
+    }
+    return a * factorial(a-1);
 }
+
 int calculate(int n, int k){
     return factorial(n) / (factorial(k) * factorial(n - k));
 }
@@ -15,37 +16,33 @@ int calculate(int n, int k){
   
 int main() 
 { 
-    // define memory for pascal_table
     printf("Enter the number of rows: ");
     int n;
     scanf("%d", &n);
     
     int **pascal_table = malloc(n * sizeof(int *));
-    for (int i = 0 ; i<= n ; i++){
+    for (int i = 0 ; i< n ; i++){
         pascal_table[i] = malloc((i+1) * sizeof(int));
     }
 
-    // add vaule to pascal_table
-    for (int line = 0; line <= n; line++) { 
+    for (int line = 0; line < n; line++) { 
         for (int i = 0; i <= line; i++) { 
             pascal_table[line][i] = calculate(line, i);
         } 
     }
-
-    // print pascal_table
-    for (int i = 0; i <= n; i++){
+    for (int i = 0; i < n; i++){
         for (int j = 0; j <= i; j++){
             printf("%d ", pascal_table[i][j]);
         }
         printf("\n");
     }
+    printf("Before memory deallocation\n");
 
-    // free memory
-    for (int i = 0; i <= n; i++){
+    for (int i = 0; i < n; i++){
         free(pascal_table[i]);
     }
     free(pascal_table);
-
+    printf("After memory deallocation");
     return 0;
   
 }
--- zadania3/zadanie2.c
+++ zadania3/zadanie2.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Function to generate random numbers between min and max (inclusive)
 int generateRandomNumber(int min, int max) {
     return (rand() % (max - min + 1)) + min;
 }
@@ -10,28 +9,26 @@ int generateRandomNumber(int min, int max) {
 int main(){
     int a = 5;
     int **p = malloc(a * sizeof(int *));
-    for (int i = 0; i <= a; i++){
+    for (int i = 0; i < a; i++){
         p[i] = malloc(a * sizeof(int));
         for (int j = 0; j <= a; j++) {
-            p[i][j] = generateRandomNumber(1, 100); // Generate random number between 1 and 100
+            p[i][j] = generateRandomNumber(1, 100);
         }
     }
 
-    // Display the matrix
-    for (int i = 0; i <= a; i++){
-        for (int j = 0; j <= a; j++) {
+    for (int i = 0; i < a; i++){
+        for (int j = 0; j < a; j++) {
             printf("%2d ", p[i][j]);
         }
         printf("\n");
     }
 
-    printf("Memory has been freed\n");
-
-    // Free memory
-    for (int i = 0; i <= a; i++){
+    for (int i = 0; i < a; i++){
         free(p[i]);
     }
+    free(p);
 
+    printf("Memory has been freed\n");
  return 0;
 }
     
